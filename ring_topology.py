24f2ca5e98a0047679fa832dcddd1b7034a5a2b0
filippomorandi <m.filippo96@hotmail.it>
2018-05-02 16:21:16 +0200

    6 switches ring topology

---
+++ ring_topology.py
@@ -0,0 +1,52 @@
+from mininet.cli import CLI
+from mininet.net import Mininet
+from mininet.node import RemoteController
+from mininet.term import makeTerm
+
+if '__main__' == __name__:
+    net = Mininet(controller=RemoteController)
+
+    c0 = net.addController('c0', port=6633)
+
+    s1 = net.addSwitch('s1')
+    s2 = net.addSwitch('s2')
+    s3 = net.addSwitch('s3')
+    s4 = net.addSwitch('s4')
+    s5 = net.addSwitch('s5')
+    s6 = net.addSwitch('s6')
+
+    h1 = net.addHost('h1')
+    h2 = net.addHost('h2')
+    h3 = net.addHost('h3')
+    h4 = net.addHost('h4')
+    h5 = net.addHost('h5')
+    h6 = net.addHost('h6')
+
+    net.addLink(s1,h1)
+    net.addLink(s2,h2)
+    net.addLink(s3,h3)
+    net.addLink(s4,h4)
+    net.addLink(s5,h5)
+    net.addLink(s6,h6)
+
+    net.addLink(s1,s2)
+    net.addLink(s2,s3)
+    net.addLink(s3,s4)
+    net.addLink(s4,s5)
+    net.addLink(s5,s6)
+    net.addLink(s6,s1) 
+
+    net.build()
+    c0.start()
+    s1.start([c0])
+    s2.start([c0])
+    s3.start([c0])
+    s4.start([c0])
+    s5.start([c0])
+    s6.start([c0])
+
+    net.startTerms()
+
+    CLI(net)
+    net.stop()
+
